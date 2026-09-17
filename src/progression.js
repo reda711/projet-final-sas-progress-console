acce3e0f4a61d198fad57661d1a47204b332af4f
Reda711 <redaalaoui711@gmail.com>
2026-09-17 12:04:22 +0100

    validerresultatfunction

--- src/progression.js
+++ src/progression.js
@@ -1,7 +1,6 @@
 const apprenants = require("./data");
 
 
-
 function normaliserNom(nom) {
     nom = nom.replace(/[^a-zA-Z\s]/g, "").trim().replace(/\s+/g, " ").toLowerCase().split(" ");
     for (let i = 0; i < nom.length; i++) {
@@ -9,4 +8,30 @@ function normaliserNom(nom) {
           }
     nom = nom.join(" ")
     return nom
+        }
+
+
+
+
+
+function validerResultat(resultat) {
+
+    if (!Number.isInteger(resultat.jour) || resultat.jour < 1 || resultat.jour > 7) {
+        return false
+    }
+    if (!Number.isInteger(resultat.totalExercices) || resultat.totalExercices <= 0) {
+        return false
+    }
+    if (!Number.isInteger(resultat.exercicesTermines) || resultat.exercicesTermines < 0) {
+        return false
+    }
+    if (resultat.exercicesTermines > resultat.totalExercices) {
+        return false
+    }
+    if (typeof resultat.challengeTermine !== "boolean"){
+        return false
+    }
+
+    return true
 }
+module.exports = { validerResultat };
