724bde6377fce4e54e0cdc53787eba5fbe2ffc1e
Reda711 <redaalaoui711@gmail.com>
2026-09-18 10:11:01 +0100

    ajouterapprenantfunction

--- src/progression.js
+++ src/progression.js
@@ -1,5 +1,5 @@
 const apprenants = require("./data");
-
+const prompt = require("prompt-sync")();
 
 function normaliserNom(nom) {
     nom = nom.replace(/[^a-zA-Z\s]/g, "").trim().replace(/\s+/g, " ").toLowerCase().split(" ");
@@ -10,28 +10,61 @@ function normaliserNom(nom) {
     return nom
         }
 
-
-
-
-
 function validerResultat(resultat) {
 
     if (!Number.isInteger(resultat.jour) || resultat.jour < 1 || resultat.jour > 7) {
+        console.log("Please enter a valid day ");
         return false
     }
-    if (!Number.isInteger(resultat.totalExercices) || resultat.totalExercices <= 0) {
-        return false
-    }
-    if (!Number.isInteger(resultat.exercicesTermines) || resultat.exercicesTermines < 0) {
-        return false
-    }
-    if (resultat.exercicesTermines > resultat.totalExercices) {
+
+    if (!Number.isInteger(resultat.exercicesTermines) || resultat.exercicesTermines < 0 || resultat.exercicesTermines > resultat.totalExercices) {
+        console.log("Invalide , Please enter the number of exercices completed ");
         return false
     }
     if (typeof resultat.challengeTermine !== "boolean"){
+        console.log("your answer is not a boolean");
         return false
     }
 
     return true
 }
 module.exports = { validerResultat };
+
+function ajouterApprenant() {
+    let id = Number(prompt("Please enter your id : "));
+    while (!Number.isInteger(id) || id <= 0 ) {
+        console.log("Please enter a valid ID");
+        id = Number(prompt("Please enter your id : "));
+    }
+    let alreadyexist = apprenants.some(apprenant => apprenant.id === id)  
+    while(alreadyexist){
+        console.log(`this id already exist`);
+        id = Number(prompt("Please enter your id : "));
+        alreadyexist = apprenants.some(apprenant => apprenant.id === id);
+    }
+
+    let nomComplet = prompt("Please enter your full name : ");
+
+    while (!nomComplet || nomComplet.trim() === "") {
+        console.log("Please enter a valid name");
+        nomComplet = prompt("Please enter your full name : ");
+    }
+
+    let ville = prompt("Please enter your city: ");
+
+    while (!ville || ville.trim() === "") {
+        console.log("Please enter a valid city");
+        ville = prompt("Please enter your city: ");
+    }
+
+    let apprenant = {
+        id: id,
+        nomComplet: normaliserNom(nomComplet),
+        ville : ville.charAt(0).toUpperCase() + ville.slice(1),
+        resultats: []
+    };
+
+    apprenants.push(apprenant);
+
+    return `the new learner has been created`;
+}
